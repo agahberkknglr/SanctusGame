551556c83160c895cf12390016c087eec8c412a3
Perplexed13 <yedekguler@gmail.com>
2023-03-21 10:07:29 +0300

    ball script

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private bool stickToPlayer;
+    [SerializeField] private Transform transformPlayer;
+    //unityde transformplayera forward1i atadık.
+
+    
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+
+        if (!stickToPlayer) //false
+        {
+            float distanceToPlayer = Vector3.Distance(transformPlayer.position, transform.position);
+            //Debug.Log(distanceToPlayer);
+            //transformplayer forward1. normal transform position da topun positionunu buluyor.
+
+            if (distanceToPlayer<0.5)
+            {
+                stickToPlayer = true;
+            }
+
+        }
+        else //true 
+        {
+            transform.position = transformPlayer.position;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# soccer-game
+advanced game course
