b99737b04ebc0cd2b55f9d6421288bdaa5a086b5
kamilcobann <kamil.cobann.0@icloud.com>
2023-04-05 04:52:28 +0300

    SFX and Game Environment Added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ using TMPro;
 public class Player: MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textScore;
+
+    [SerializeField] private TextMeshProUGUI textGoal;
     private StarterAssetsInputs starterAssetsInputs;
     private Animator animator;
     private Ball ballAttachedToPlayer;
@@ -14,6 +16,13 @@ public class Player: MonoBehaviour
     private const int LAYER_SHOOT = 1;
     private CharacterController characterController;
     private int myScore, otherScore;
+    private float goalTextColorAlpha;  
+    private AudioSource soundBallDribble;
+    private AudioSource soundBallKick;
+    private AudioSource soundCheer;
+    private AudioSource soundGoalCheer;
+    private float distanceSinceLastDribble;
+
     public Ball BallAttachedToPlayer { get => ballAttachedToPlayer; set=> ballAttachedToPlayer = value;}
     
     void Start()
@@ -21,12 +30,20 @@ public class Player: MonoBehaviour
         starterAssetsInputs = GetComponent<StarterAssetsInputs>();
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
+
+        soundBallKick = GameObject.Find("Sound/ball-kick").GetComponent<AudioSource>();
+        soundBallDribble = GameObject.Find("Sound/ball-dribble").GetComponent<AudioSource>();
+        soundCheer = GameObject.Find("Sound/ambient-cheer").GetComponent<AudioSource>();
+        soundGoalCheer= GameObject.Find("Sound/goal-cheer").GetComponent<AudioSource>();
+
+        soundCheer.Play();
+
+        textGoal.fontSize = 0f;
     }
 
     void Update()
     {
         float speed = new Vector3(characterController.velocity.x, 0, characterController.velocity.z).magnitude;
-
         if(starterAssetsInputs.shoot)
         {
             starterAssetsInputs.shoot = false;
@@ -38,6 +55,9 @@ public class Player: MonoBehaviour
         {
             if (BallAttachedToPlayer != null && Time.time - timeShot> 0.2)
             {
+
+                soundBallKick.Play();
+
                 BallAttachedToPlayer.stickToPlayer = false;
                 Rigidbody rigidbody = ballAttachedToPlayer.transform.gameObject.GetComponent<Rigidbody>();
                 Vector3 shootdirection = transform.forward;
@@ -55,6 +75,24 @@ public class Player: MonoBehaviour
         {
             animator.SetLayerWeight(LAYER_SHOOT, Mathf.Lerp(animator.GetLayerWeight(LAYER_SHOOT), 0f, Time.deltaTime * 10f));
         }
+
+        if(goalTextColorAlpha>0)
+        {
+            goalTextColorAlpha -= Time.deltaTime;
+            textGoal.alpha = goalTextColorAlpha;
+            textGoal.fontSize = 200 - (goalTextColorAlpha * 1-0);
+        }
+
+        if(BallAttachedToPlayer != null)
+        {
+            distanceSinceLastDribble += speed * Time.deltaTime;
+            if(distanceSinceLastDribble > 1)
+            {
+                Debug.Log("DRIBBLE");
+                soundBallDribble.Play();
+                distanceSinceLastDribble = 0;
+            }
+        }
         
     }
 
@@ -71,7 +109,8 @@ public class Player: MonoBehaviour
     }
 
     private void UpdateScore()
-    {
+    {soundGoalCheer.Play();
        textScore.text = "P1 : "+ myScore.ToString() +" - P2 : "+otherScore.ToString(); 
+       goalTextColorAlpha = 1f;
     }
 }
